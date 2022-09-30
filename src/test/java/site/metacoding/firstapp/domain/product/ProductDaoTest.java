4ff3b338427124285d2eabeeb1740dc274382332
Sylar <kmc42692@naver.com>
2022-09-30 09:29:21 +0900

    ProductDao findAll테스트 완료

--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -2,15 +2,15 @@ package site.metacoding.firstapp.domain.product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 import site.metacoding.firstapp.config.MyBatisConfig;
-
 
 @Import(MyBatisConfig.class) // MyBatisTest가 MyBatisConfig를 못읽음
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실DB사용
@@ -30,6 +30,18 @@ public class ProductDaoTest {
 
         // then(검증)
         assertEquals("바나나", productPS.getProductName());
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Product> productListPS = productDao.findAll();
+        System.out.println(productListPS.get(0).getProductName());
+
+        // then
+        assertEquals(2, productListPS.size());
     }
 
 }
