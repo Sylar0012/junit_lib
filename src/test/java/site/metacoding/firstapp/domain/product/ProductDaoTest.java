223e3fe2651920658064c263e2b6a9d925b5a39a
Sylar <kmc42692@naver.com>
2022-09-30 11:18:28 +0900

    ProductDao Junit 테스트 완료

--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -1,6 +1,7 @@
 package site.metacoding.firstapp.domain.product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
@@ -44,4 +45,64 @@ public class ProductDaoTest {
         assertEquals(2, productListPS.size());
     }
 
+    @Test
+    public void insert_test() {
+        // given
+
+        Product product = new Product("수박", 1000, 100);
+
+        // when
+        int result = productDao.insert(product);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void update_test() {
+        // given
+        Integer productId = 1;
+        String productName = "수박";
+        Integer productPrice = 1000;
+        Integer productQty = 100;
+
+        Product product = new Product(productName, productPrice, productQty);
+        product.setProductId(productId);
+
+        // verify
+        Product productPS = productDao.findById(product.getProductId());
+        assertTrue(productPS == null ? false : true);
+
+        // when
+
+        // product = [id=1, productName="수박", productPrice = 1000, productQty = 100]
+        // productPS = [id=1, productName="바나나", productPrice = 3000, productQty = 98,
+        // createdAt = 2022.09.29]
+        productPS.update(product);
+        // productPS = [id=1, productName="수박", productPrice = 1000, productQty = 100,
+        // createdAt = 2022.09.29]
+        int result = productDao.update(productPS);
+        // productPS = [id=1, productName="수박", productPrice = 1000, productQty = 100,
+        // createdAt = 2022.09.29] 업데이트
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void delete_test() {
+        // given
+        Integer productId = 2;
+
+        // verify
+        Product productPS = productDao.findById(productId);
+        assertTrue(productPS == null ? false : true);
+
+        // when
+        int result = productDao.deleteById(productId);
+
+        // then
+        assertEquals(1, result);
+
+    }
 }
